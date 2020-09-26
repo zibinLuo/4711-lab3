ce83019655a23ba11a8084650cb7581d7b60ef7e
zibinLuo <1824912827@qq.com>
2020-09-26 18:56:33 +0800

    Added an index page

--- Student.php
+++ Student.php
@@ -1,17 +1,50 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        echo'zibinLuo';
-        ?>
-    </body>
-</html>
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Student
+ *
+ * @author Administrator
+ */
+class Student {
+    //put your code here<!DOCTYPE html>
+
+        function __construct() {
+$this->surname = '';
+$this->first_name = '';
+$this->status='';
+$this->emails = array();
+$this->grades = array();
+}
+
+function add_email($which,$address) {
+$this->emails[$which] = $address;
+}
+function add_grade($grade) {
+$this->grades[] = $grade;
+}
+
+function average() {
+$total = 0;
+foreach ($this->grades as $value)
+$total += $value;
+return $total / count($this->grades);
+}
+
+function toString() {
+$result = $this->first_name . ' ' . $this->surname;
+$result .= ' ('.$this->average().")\n";
+$result .= "status: ".$this->status."\n";
+foreach($this->emails as $which=>$what)
+$result .= $which . ': '. $what. "\n";
+$result .= "\n";
+return '<pre>'.$result.'</pre>';
+}
+
+
+}
